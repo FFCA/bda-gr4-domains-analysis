dc8f6ec0a00e7057482a9c15484f833f35b1d689
Lucas Larisch <lucas.larisch@googlemail.com>
2021-06-07 00:04:39 +0200

    Moved dig-command to express application.

--- services/dig-microservice/src/index.ts
+++ services/dig-microservice/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello dig-world!");
+import express from 'express';
+import { exec } from 'child_process';
+
+const app = express();
+const host = 'localhost';
+const port = 8088;
+
+app.listen(port, host, () => {
+    console.log(`Server is running at ${host}:${port}`);
+
+    exec('dig google.com', (error, stdout, stderr) => {
+        console.log('1', stdout);
+        console.log('2', stderr);
+        if (error !== null) console.log(`exec error: ${error}`);
+    });
+});
