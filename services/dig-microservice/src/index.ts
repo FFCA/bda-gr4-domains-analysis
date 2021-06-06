483d3b32e79eaff00914c63b850fec7beeeda31c
Lucas Larisch <lucas.larisch@googlemail.com>
2021-06-07 01:00:12 +0200

    Implemented `dig` command as endpoint (currently only digging google.com).

--- services/dig-microservice/src/index.ts
+++ services/dig-microservice/src/index.ts
@@ -5,12 +5,24 @@ const app = express();
 const host = 'localhost';
 const port = 8088;
 
+const jsonDig = (dig: string) => {
+    // TODO Implement
+    return 'This string will be converted to json:' + dig;
+};
+
 app.listen(port, host, () => {
     console.log(`Server is running at ${host}:${port}`);
 
-    exec('dig google.com', (error, stdout, stderr) => {
-        console.log('1', stdout);
-        console.log('2', stderr);
-        if (error !== null) console.log(`exec error: ${error}`);
+    app.get('/dig', (req, res) => {
+        // if (!req.query.ip) {
+        //     const msg = 'Request is lackig from the required parameter `ip`.';
+        //     res.status(422).send(msg);
+        // } else {
+        exec('dig google.com', (error, stdout, stderr) => {
+            if (stdout) res.send(!req.query.raw ? jsonDig(stdout) : stdout);
+            else if (stderr) res.status(500).send(stderr);
+            if (error !== null) res.status(500).send(error);
+        });
+        // }
     });
 });
