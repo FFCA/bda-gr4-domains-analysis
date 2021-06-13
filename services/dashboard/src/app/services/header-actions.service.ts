9c1ad29b4351c403302d8477529679732d09f28a
Lucas Larisch <lucas.larisch@googlemail.com>
2021-06-13 17:07:46 +0200

    #9: Also allow FontAwesome as action icons/added dig action button.

--- services/dashboard/src/app/services/header-actions.service.ts
+++ services/dashboard/src/app/services/header-actions.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
 import { I18nService } from './i18n.service';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { faTerminal } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { MatDialog } from '@angular/material/dialog';
+
+/**
+ * Action item including styling/descriptive properties to be displayed.
+ */
+type Action = {
+    translationKey: string;
+    color: string;
+    onClick: () => void;
+    icon?: string;
+    faIcon?: IconDefinition;
+};
 
 /**
  * Service for globally defining header actions in order to use them in both the header and the nav menu (small screens).
@@ -17,12 +31,18 @@ export class HeaderActionsService {
     /**
      * List of actions including styling/descriptive properties to be displayed.
      */
-    readonly actionItems = [
+    readonly actionItems: Action[] = [
         {
             icon: 'language',
             translationKey: 'header.action.language',
             color: 'primary',
             onClick: () => this.i18n.mockLanguageSwitch(),
+        },
+        {
+            faIcon: faTerminal,
+            translationKey: 'header.action.digMs',
+            color: 'primary',
+            onClick: () => console.log("to be implemented"),
         },
     ].map((a) => {
         return {
@@ -43,8 +63,13 @@ export class HeaderActionsService {
     /**
      * @param media Media Matcher to be used in order to initialize the service's mobile query.
      * @param i18n Injected i18n service.
+     * @param dialog Injected Material dialog service.
      */
-    constructor(media: MediaMatcher, private readonly i18n: I18nService) {
+    constructor(
+        media: MediaMatcher,
+        private readonly i18n: I18nService,
+        private readonly dialog: MatDialog
+    ) {
         // See bootstrap breakpoints
         this.mobileQuery = media.matchMedia('(max-width: 576px)');
     }
