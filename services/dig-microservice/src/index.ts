3a0fec102867ed307e9aefbfe82abb8800c6ae64
Lucas Larisch <lucas.larisch@googlemail.com>
2021-06-13 21:54:09 +0200

    #6: Check of characters for string to be digged (=> security).

--- services/dig-microservice/src/index.ts
+++ services/dig-microservice/src/index.ts
@@ -14,6 +14,21 @@ const jsonDig = (dig: string) => {
 };
 
 /**
+ * Checks whether a string value contains illegal characters and if so, returns a warning message.
+ *
+ * @param value Value to be checked.
+ * @return Message concerning illegal character or undefined if no illegal character has been found.
+ */
+const invalidMsg = (value: string): string | undefined => {
+    const regex = /[a-z0-9.:-_]/;
+    const chars = [...value.trim()];
+    const illegalChar = chars.find((c) => !c.match(regex));
+    if (illegalChar) {
+        return `Character '${illegalChar}' does not match ${regex} and is thus not considered to be secure.\nCommand will not be executed`;
+    } else return undefined;
+};
+
+/**
  * Starts the application at the specified host/port.
  */
 app.listen(port, host, () => {
@@ -26,16 +41,27 @@ app.listen(port, host, () => {
      * param is performed and, if valid, the result is sent back.
      */
     app.get('/:dig', (req, res) => {
-        exec(`dig ${req.params.dig}`, (error, stdout, stderr) => {
-            console.log('processing: ' + req.params.dig);
-            if (stdout) {
-                res.send({
-                    answer: !req.query.raw ? jsonDig(stdout) : stdout,
-                    digged: req.params.dig,
-                    timestamp: new Date(),
-                });
-            } else if (stderr) res.status(500).send(stderr);
-            else if (error !== null) res.status(500).send(error);
-        });
+        const value = req.params.dig.trim();
+        const warningMsg = invalidMsg(value);
+
+        if (!warningMsg) {
+            console.log('processing: ' + value);
+            exec(`dig ${value}`, (error, stdout, stderr) => {
+                if (stdout) {
+                    res.send({
+                        answer: !req.query.raw ? jsonDig(stdout) : stdout,
+                        digged: value,
+                        timestamp: new Date(),
+                    });
+                } else if (stderr) {
+                    res.status(500).send(stderr);
+                } else if (error !== null) {
+                    res.status(500).send(error);
+                }
+            });
+        } else {
+            res.status(400).send(warningMsg);
+            console.warn(warningMsg);
+        }
     });
 });
