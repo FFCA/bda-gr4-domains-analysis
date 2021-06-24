fd4350620160588da2e1d2652f5ef92cd60a4c7a
Lucas Larisch <lucas.larisch@googlemail.com>
2021-06-24 04:28:10 +0200

    #1: Mounted Volume (csv data).

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 extension=.csv
 csvFile=real_domains$extension
 basename="$(dirname "$0")/"
-sparkdir=$basename"services/pyspark-analyse/"
+datadir=$basename"data/"
 
 # Error case: file stated as param exists but is not a csv => exit with information
 if ! [ -z $1 ] && [ -e $1 ] && [ "${1: -4}" != $extension ]; then
@@ -9,18 +9,19 @@ if ! [ -z $1 ] && [ -e $1 ] && [ "${1: -4}" != $extension ]; then
   exit 1
 # Case: File stated as param is valid csv => copy file to the repository's root dir making sure it is named correctly
 elif ! [ -z $1 ] && [ -e $1 ]; then
-  echo "Copying file" $1 "to" $sparkdir$csvFile "(updates existing file)"
-  cp $1 $sparkdir$csvFile
+  echo "Copying file" $1 "to" $datadir$csvFile "(updates existing file)"
+  mkdir -p $datadir
+  cp $1 $datadir$csvFile
 # Case: No or no existing file stated as param but csv could be found at the required location
-elif [ -e $sparkdir$csvFile ]; then
-  echo "Existing" $sparkdir$csvFile "will be used!"
+elif [ -e $datadir$csvFile ]; then
+  echo "Existing" $datadir$csvFile "will be used!"
 # No data at the repository's root and no existing file stated as param  => exit with information
 else
-  echo "Failed to find" $sparkdir$csvFile"!"
+  echo "Failed to find" $datadir$csvFile"!"
   echo "Note: If you started this script with a file path as argument, this path does not seem to exist."
   echo "This script makes sure the application is only called if the required data exists. You have two options:"
   echo "  1. Download the data and call this shell script with the path to the downloaded data, e.g. (called from root): sh setup.sh <path to your csv>"
-  echo "  2. Download the data and store it manually as:" $sparkdir$csvFile "(relative path to the directory this script has been run from)"
+  echo "  2. Download the data and store it manually as:" $datadir$csvFile "(relative path to the directory this script has been run from)"
   echo "In case of questions, check the assignment specification in Moodle."
   exit 1
 fi
