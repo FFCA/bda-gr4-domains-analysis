557f527980575cfc94221a3182780a73ddbca631
FFCA <56739691+FFCA@users.noreply.github.com>
2021-06-26 23:07:38 +0200

    Update init.sql

--- services/postgres-db/init.sql
+++ services/postgres-db/init.sql
@@ -2,5 +2,9 @@ CREATE TABLE domain (
     id SERIAL PRIMARY KEY,
     top_level_domain VARCHAR(255) UNIQUE NOT NULL,
     mx_record TEXT[] NULL,
-    a_record TEXT[] NULL
+    a_record TEXT[] NULL,
+	a_record_checked TEXT[] NULL,
+	mx_record_checked TEXT[] NULL,
+	redirection VARCHAR(255) NULL,
+	status_code VARCHAR(255) NULL
 );
