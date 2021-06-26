eb8ee024f4ca1e3f44af524985a4baff7f494925
Lucas Larisch <lucas.larisch@googlemail.com>
2021-06-26 23:43:19 +0200

    #2: Added alternate ETL script with: (db) "overwrite" => "append", "No information" => None, comma-separated str => array.

--- services/postgres-db/init.sql
+++ services/postgres-db/init.sql
@@ -1,27 +1,43 @@
-CREATE TABLE domain (
-    id SERIAL PRIMARY KEY,
-    top_level_domain VARCHAR(255) UNIQUE NOT NULL,
-    mx_record TEXT[] NULL,
-    a_record TEXT[] NULL,
-	a_record_checked TEXT[] NULL,
-	mx_record_checked TEXT[] NULL,
-	redirection VARCHAR(255) NULL,
-	status_code VARCHAR(255) NULL
+-- Creation of table(s):
+
+CREATE TABLE domain
+(
+    id                SERIAL PRIMARY KEY,
+    top_level_domain  VARCHAR(255) UNIQUE NOT NULL,
+    mx_record         VARCHAR(255)[] NULL,
+    a_record          VARCHAR(255)[] NULL,
+    -- TODO: Discuss VARCHAR/TEXT and insertion in general
+    a_record_checked  TEXT[] NULL,
+    mx_record_checked TEXT[] NULL,
+    redirection       VARCHAR(255) NULL,
+    status_code       VARCHAR(255) NULL
 );
+
+-- Creation of notification function:
 
 CREATE FUNCTION notify_trigger() RETURNS trigger AS $$
 DECLARE
 BEGIN
-  PERFORM pg_notify('watch_domain', TG_TABLE_NAME);
+  PERFORM
+pg_notify('watch_domain', TG_TABLE_NAME);
 RETURN NULL;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
-CREATE TRIGGER insert_trigger AFTER INSERT ON domain
+-- Creation of triggers:
+
+CREATE TRIGGER insert_trigger
+    AFTER INSERT
+    ON domain
     FOR EACH ROW EXECUTE PROCEDURE notify_trigger();
 
-CREATE TRIGGER update_trigger AFTER UPDATE ON domain
+CREATE TRIGGER update_trigger
+    AFTER UPDATE
+    ON domain
     FOR EACH ROW EXECUTE PROCEDURE notify_trigger();
 
-CREATE TRIGGER delete_trigger AFTER DELETE ON domain
+CREATE TRIGGER delete_trigger
+    AFTER DELETE
+    ON domain
     FOR EACH ROW EXECUTE PROCEDURE notify_trigger();
