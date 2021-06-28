0f81c473bda9d96e27820e8c8a9c5f1830450547
Lucas Larisch <lucas.larisch@googlemail.com>
2021-06-28 03:31:50 +0200

    #7: Added script (+ tables) for collecting data based on data from csv.

--- services/postgres-db/init.sql
+++ services/postgres-db/init.sql
@@ -1,15 +1,35 @@
--- Creation of table(s):
+-- Creation of tables:
 
 CREATE TABLE domain
 (
-    top_level_domain  VARCHAR(255) PRIMARY KEY,
-    mx_record         VARCHAR(255)[] NULL,
-    a_record          VARCHAR(255)[] NULL
+    top_level_domain VARCHAR(255) PRIMARY KEY,
+    mx_record        VARCHAR(255)[] NULL,
+    a_record         VARCHAR(255)[] NULL
 );
 
-CREATE TABLE domain_enhanced
+CREATE TABLE domain_enhanced_based_on_existing_data
 (
-    top_level_domain  VARCHAR(255) REFERENCES domain(top_level_domain) PRIMARY KEY,
+    top_level_domain  VARCHAR(255) REFERENCES domain (top_level_domain) PRIMARY KEY,
+    a_record_count    INTEGER NOT NULL,
+    mx_record_count   INTEGER NOT NULL,
+    mx_uses_localhost BOOLEAN NOT NULL
+);
+
+CREATE TABLE a_record_count_global
+(
+    a_record VARCHAR(255) PRIMARY KEY,
+    count    INTEGER NOT NULL
+);
+
+CREATE TABLE mx_record_count_global
+(
+    mx_record VARCHAR(255) PRIMARY KEY,
+    count     INTEGER NOT NULL
+);
+
+CREATE TABLE domain_enhanced -- TODO: new table to be used / adjusted
+(
+    top_level_domain  VARCHAR(255) REFERENCES domain (top_level_domain) PRIMARY KEY,
     a_record_checked  VARCHAR(255)[] NULL,
     mx_record_checked VARCHAR(255)[] NULL,
     redirection       VARCHAR(255) NULL,
