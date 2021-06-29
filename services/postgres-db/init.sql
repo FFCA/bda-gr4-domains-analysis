c32b0f541578b4511a8786e63983ed47643d8c9c
Lucas Larisch <lucas.larisch@googlemail.com>
2021-06-29 12:24:25 +0200

    #22: (Test) removed primary keys from DB in order to avoid time-consuming lookups.

--- services/postgres-db/init.sql
+++ services/postgres-db/init.sql
@@ -2,14 +2,14 @@
 
 CREATE TABLE domain
 (
-    top_level_domain VARCHAR(255) PRIMARY KEY,
+    top_level_domain VARCHAR(255) NOT NULL, -- PRIMARY KEY,
     mx_record        VARCHAR(255)[] NULL,
     a_record         VARCHAR(255)[] NULL
 );
 
 CREATE TABLE domain_enhanced_based_on_existing_data
 (
-    top_level_domain  VARCHAR(255) REFERENCES domain (top_level_domain) PRIMARY KEY,
+    top_level_domain VARCHAR(255) NOT NULL, -- REFERENCES domain (top_level_domain) PRIMARY KEY,
     a_record_count    INTEGER NOT NULL,
     mx_record_count   INTEGER NOT NULL,
     mx_uses_localhost BOOLEAN NOT NULL
@@ -17,19 +17,19 @@ CREATE TABLE domain_enhanced_based_on_existing_data
 
 CREATE TABLE a_record_count_global
 (
-    a_record VARCHAR(255) PRIMARY KEY,
+    a_record VARCHAR(255) NOT NULL -- PRIMARY KEY,
     count    INTEGER NOT NULL
 );
 
 CREATE TABLE mx_record_count_global
 (
-    mx_record VARCHAR(255) PRIMARY KEY,
+    mx_record VARCHAR(255) NOT NULL -- PRIMARY KEY,
     count     INTEGER NOT NULL
 );
 
 CREATE TABLE domain_enhanced -- TODO: new table to be used / adjusted
 (
-    top_level_domain  VARCHAR(255) REFERENCES domain (top_level_domain) PRIMARY KEY,
+    top_level_domain  VARCHAR(255) NOT NULL -- REFERENCES domain (top_level_domain) PRIMARY KEY,
     a_record_checked  VARCHAR(255)[] NULL,
     mx_record_checked VARCHAR(255)[] NULL,
     redirection       VARCHAR(255) NULL,
